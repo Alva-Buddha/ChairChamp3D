09327cda1ba62b0ea3932b17e4d8ab0fd55caf0f
pharland <110207014+pharland@users.noreply.github.com>
2024-08-02 00:03:37 +1000

    Added template for powerup VFX
    - To replace vfx, update the "" script on the "" game object
    - Prefab is in front of the player
    - Spawns the VFX depending on the power
    - uses test vfx prefabs for now
    
    - Also updated the levels so that no power is selected by default

--- ChairChamp3D/Assets/Scripts/Powers/Power.cs
+++ ChairChamp3D/Assets/Scripts/Powers/Power.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Power : MonoBehaviour
 {
+    //Variables for VFX
+    public float vfxDuration = 4f;
+    public GameObject dashVFX;
+    public GameObject stunVFX;
+    public GameObject swapVFX;
+    public GameObject pullVFX;
+
+
     //create inputManager object
     private InputManager inputManager;
     private AudioManager audioManager;
@@ -193,8 +201,24 @@ public class Power : MonoBehaviour
 
         // Set dash timer to cooldown
         dashTimer = dashCooldown;
+
         // Play sound effect
         audioManager.PlayDashSFX();
+
+        // Play VFX
+        StartCoroutine(PowerupVFX(dashVFX));
+    }
+
+    IEnumerator PowerupVFX(GameObject vfx)
+    {
+        //Create VFX
+        Instantiate(vfx, transform.position, transform.rotation);
+
+        // Set timer for VFX to be destroyed
+        yield return new WaitForSeconds(vfxDuration);
+
+        // Destroy VFX
+        Destroy(vfx);
     }
 
     // Coroutine to reset the Rigidbody's velocity after a specified time
@@ -236,8 +260,12 @@ public class Power : MonoBehaviour
         }
         // Set pull timer to cooldown
         pullTimer = pullCooldown;
+
         // Play sound effect
         audioManager.PlayPullSFX();
+
+        // Play VFX
+        StartCoroutine(PowerupVFX(pullVFX));
     }
 
     private IEnumerator PullTargetOverTime(Transform target, float duration)
@@ -301,6 +329,9 @@ public class Power : MonoBehaviour
 
         // Play sound effect
         audioManager.PlayStunSFX();
+
+        // Play VFX
+        StartCoroutine(PowerupVFX(stunVFX));
     }
 
     // Coroutine to set target object velocity to zero and lock constraints for stunTime
@@ -352,6 +383,9 @@ public class Power : MonoBehaviour
 
         // Play sound effect
         audioManager.PlaySwapSFX();
+
+        // Play VFX
+        StartCoroutine(PowerupVFX(swapVFX));
     }
 }
 
